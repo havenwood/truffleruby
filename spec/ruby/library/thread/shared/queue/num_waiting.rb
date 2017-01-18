bfb0f8b49cd1e23878deb1f98f2f300fd0614c1e
Benoit Daloze <eregontp@gmail.com>
2017-01-18 16:21:15 +0100

    Add a note on why a spec fails

--- spec/ruby/library/thread/shared/queue/num_waiting.rb
+++ spec/ruby/library/thread/shared/queue/num_waiting.rb
@@ -1,4 +1,10 @@
 describe :queue_num_waiting, shared: true do
+  # Note: this fails because the threads Array becomes shared,
+  # which uses the SafepointManager which interrupts() the
+  # first subthread, which after goes back to waiting to pop.
+  # But in between, the interrupted thread is not sleeping
+  # and is not actually waiting on the Queue condition.
+  # Keeping our own count of num_waiting would fix this.
   it "reports the number of threads waiting on the queue" do
     q = @object.call
     threads = []
