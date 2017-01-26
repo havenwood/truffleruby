342ea269d06508ea21cb460fc647aee103aa180a
Benoit Daloze <eregontp@gmail.com>
2017-01-26 15:35:44 +0100

    Add a test for concurrent appends

---
+++ bench/test_concurrent_appends.rb
@@ -0,0 +1,26 @@
+Thread.new {}
+
+ARY = ary = []
+
+p Truffle::Debug.array_storage(ary) if defined?(Truffle)
+
+# ARY << 1
+# p Truffle::Debug.array_storage(ary) if defined?(Truffle)
+
+T = 100 # 4*4
+N = 1000
+
+T.times.map {
+  Thread.new {
+    i = 0
+    while i < N
+      ARY << i
+      i += 1
+    end
+  }
+}.each(&:join)
+
+p Truffle::Debug.array_storage(ary) if defined?(Truffle)
+puts "Expected: #{T*N}"
+p ARY.size
+# p ARY
