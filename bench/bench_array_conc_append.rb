10ac526397cd1a0677cf5c697c9bb4914e339282
Benoit Daloze <eregontp@gmail.com>
2017-01-31 19:46:31 +0100

    Add concurrent appends benchmark

---
+++ bench/bench_array_conc_append.rb
@@ -0,0 +1,90 @@
+N = 10_000_000 / 100
+N_THREADS = 4
+ary = []
+
+Thread.abort_on_exception = true
+
+unless defined?(Truffle)
+  module Truffle
+    module Array
+      def self.set_strategy(ary, strategy)
+        ary
+      end
+    end
+    module Debug
+      def self.array_storage(ary)
+        ary.class.to_s
+      end
+    end
+  end
+end
+
+def setup(name)
+  eval <<EOR
+def bench_#{name}(ary)
+  i = 0
+  while i < N
+    ary << i
+    i += 1
+  end
+  ary
+end
+# alias bench bench_#{name}
+# :bench_#{name}
+EOR
+end
+
+THREADS = N_THREADS.times.map {
+  q = Queue.new
+  ret = Queue.new
+  Thread.new {
+    while job = q.pop
+      ret.push job.call
+    end
+  }
+  [q, ret]
+}
+
+def measure(input, name)
+  meth = setup(name)
+  n = 100
+  results = eval <<EOR
+  n.times.map do
+    input.clear
+    input << 0
+
+    t0 = Process.clock_gettime(Process::CLOCK_MONOTONIC, :nanosecond)
+    THREADS.each { |q,ret| q.push -> { #{meth}(input) } }
+    THREADS.each { |q,ret| ret.pop }
+    t1 = Process.clock_gettime(Process::CLOCK_MONOTONIC, :nanosecond)
+    (t1-t0) / 1000
+  end
+EOR
+
+  [results.min, results.sort[results.size/2], results.max]
+  # results.min
+end
+
+setup(:first)
+p bench_first(ary).size
+
+Truffle::Array.set_strategy(ary, :Synchronized)
+puts Truffle::Debug.array_storage(ary)
+p measure(ary, :synchd)
+p measure(ary, :synchd)
+
+Truffle::Array.set_strategy(ary, :ReentrantLock)
+puts Truffle::Debug.array_storage(ary)
+p measure(ary, :reentrant)
+
+Truffle::Array.set_strategy(ary, :CustomLock)
+puts Truffle::Debug.array_storage(ary)
+p measure(ary, :custom)
+
+Truffle::Array.set_strategy(ary, :StampedLock)
+puts Truffle::Debug.array_storage(ary)
+p measure(ary, :stamped)
+
+Truffle::Array.set_strategy(ary, :LayoutLock)
+puts Truffle::Debug.array_storage(ary)
+p measure(ary, :layout)
