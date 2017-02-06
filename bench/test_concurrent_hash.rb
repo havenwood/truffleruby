33c3a3e507a8fbabde8db6907500c6e5ce0cf0a2
Benoit Daloze <eregontp@gmail.com>
2017-02-06 16:40:26 +0100

    Update test to allow repeating it multiple times

--- bench/test_concurrent_hash.rb
+++ bench/test_concurrent_hash.rb
@@ -1,19 +1,37 @@
 Thread.abort_on_exception = true
 require_relative 'common'
 
-N_THREADS = 4
+N_THREADS = 50
 
 h = Hash.new { |h,k| sleep 0.1; h[k] = [] }
+q = Queue.new
 
 threads = N_THREADS.times.map { |t|
+  work = Queue.new
   Thread.new {
-    h[0] << t
+    while work.pop
+      Thread.pass until $go
+      h[0] << t
+      q << :ok
+    end
   }
+  work
 }
 
-threads.each(&:join)
+20.times do
+  $go = false
+  threads.each { |t| t << :work }
+  sleep 0.01
+  $go = true
 
-p h
-p h.size
-puts Truffle::Debug.array_storage(h[0])
-p h[0].size
+  threads.each { q.pop }
+
+  p h
+  p h.size
+  puts Truffle::Debug.array_storage(h[0])
+  p h[0].size
+  raise h[0].to_s unless h[0].size == N_THREADS
+
+  h.clear
+  p h
+end
