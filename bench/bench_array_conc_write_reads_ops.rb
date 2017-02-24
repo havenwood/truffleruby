8db88f92c728056e0371ec035e0399e64cbbae40
Benoit Daloze <eregontp@gmail.com>
2017-02-24 18:19:57 +0100

    Simplify bench_array_conc_write_reads_ops to only execute with a single strategy

--- bench/bench_array_conc_write_reads_ops.rb
+++ bench/bench_array_conc_write_reads_ops.rb
@@ -4,15 +4,14 @@ require_relative 'common'
 # writes to the first N elements for per bench()
 
 N = 100
-N_THREADS = Integer(ARGV[0] || 4)
+STRATEGY = ARGV[0].to_sym || raise("First argument must be the strategy")
+N_THREADS = Integer(ARGV[1] || 4)
 READS = 1024
 raise if N > READS
 ary = READS.times.to_a * N_THREADS
 SUM = READS.times.reduce(:+)
 
-def setup(name)
-  eval <<EOR, nil, __FILE__, __LINE__
-def bench_#{name}(ary, t)
+def bench(ary, t)
   base = t * READS
   i = 0
   while i < N
@@ -29,10 +28,6 @@ def bench_#{name}(ary, t)
   end
   raise sum.to_s unless sum == SUM
   sum
-end
-# alias bench bench_#{name}
-# :bench_#{name}
-EOR
 end
 
 $run = false
@@ -53,15 +48,13 @@ THREADS = N_THREADS.times.map { |t|
   [q, ret]
 }
 
-def measure(input, name)
-  meth = setup(name)
-  n = 5
-  results = eval <<EOR
-  n.times.map do
+def measure(input)
+  n = 9
+  results = n.times.map do
     ops = 0
     t0 = Process.clock_gettime(Process::CLOCK_MONOTONIC, :nanosecond)
     $run = true
-    THREADS.each { |q,ret| q.push -> t { #{meth}(input, t) } }
+    THREADS.each { |q,ret| q.push -> t { bench(input, t) } }
     sleep 2
     $run = false
     THREADS.each { |q,ret| ops += ret.pop }
@@ -71,39 +64,10 @@ def measure(input, name)
     p ops
     ops
   end
-EOR
 
   [results.min, results.sort[results.size/2], results.max]
   # results.min
 end
 
-setup(:first)
-p bench_first(ary, 0)
-
-Truffle::Array.set_strategy(ary, :FixedSize)
-puts Truffle::Debug.array_storage(ary)
-p measure(ary, :fixed)
-
-Truffle::Array.set_strategy(ary, :Synchronized)
-puts Truffle::Debug.array_storage(ary)
-p measure(ary, :synchd)
-
-Truffle::Array.set_strategy(ary, :ReentrantLock)
-puts Truffle::Debug.array_storage(ary)
-p measure(ary, :reentrant)
-
-Truffle::Array.set_strategy(ary, :CustomLock)
-puts Truffle::Debug.array_storage(ary)
-p measure(ary, :custom)
-
-Truffle::Array.set_strategy(ary, :StampedLock)
-puts Truffle::Debug.array_storage(ary)
-p measure(ary, :stamped)
-
-Truffle::Array.set_strategy(ary, :LayoutLock)
-puts Truffle::Debug.array_storage(ary)
-p measure(ary, :layout)
-
-Truffle::Array.set_strategy(ary, :FastLayoutLock)
-puts Truffle::Debug.array_storage(ary)
-p measure(ary, :fast_layout)
+Truffle::Array.set_strategy(ary, STRATEGY)
+p measure(ary)
