c77d78e896b8d8c0d095e17c36c5d979acb8e5d0
Arie Tal <arietal@Aries-MacBook-Pro-4.local>
2017-02-28 20:18:56 +0200

    a file

---
+++ truffleruby/src/main/java/org/truffleruby/core/array/layout/GetTransitioningThreadStateNode.java
@@ -0,0 +1,36 @@
+package org.truffleruby.core.array.layout;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.truffleruby.language.RubyNode;
+
+import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.NodeChild;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.object.DynamicObject;
+
+@NodeChild("array")
+public abstract class GetTransitioningThreadStateNode extends RubyNode {
+
+    public static GetTransitioningThreadStateNode create() {
+        return GetTransitioningThreadStateNodeGen.create(null);
+    }
+
+    public abstract AtomicInteger executeGetTransitioningThreadState(DynamicObject array);
+
+    @Specialization(guards = "getCurrentThread(array) == cachedThread", limit = "1")
+    protected AtomicInteger cachedThread(DynamicObject array,
+            @Cached("getCurrentThread(array)") ThreadWithDirtyFlag cachedThread) {
+        return cachedThread.getTransitioningThreadState(array);
+    }
+
+    @Specialization(contains = "cachedThread")
+    protected AtomicInteger getTransitioningThreadState(DynamicObject array) {
+        return getCurrentThread(array).getTransitioningThreadState(array);
+    }
+
+    protected ThreadWithDirtyFlag getCurrentThread(DynamicObject array) {
+        return (ThreadWithDirtyFlag) Thread.currentThread();
+    }
+
+}
