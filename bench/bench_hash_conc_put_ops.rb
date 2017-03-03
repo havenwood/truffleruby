8aa4960152028546b453e88c8a283048dfe3453d
Benoit Daloze <eregontp@gmail.com>
2017-03-03 16:45:06 +0100

    Use new benchmark harness

--- bench/bench_hash_conc_put_ops.rb
+++ bench/bench_hash_conc_put_ops.rb
@@ -1,6 +1,5 @@
 require_relative 'common'
 
-N_THREADS = Integer(ARGV[0])
 PUTS = 1000
 
 def bench(h, t)
@@ -12,52 +11,7 @@ def bench(h, t)
   end
 end
 
-11.times { |i| $un = $go = i }
-
-$run = false
-
-THREADS = N_THREADS.times.map { |t|
-  q = Queue.new
-  ret = Queue.new
-  Thread.new {
-    while job = q.pop
-      Thread.pass until $go
-      ops = 0
-      while $run
-        job.call(t)
-        ops += 1
-      end
-      ret.push ops
-    end
-  }
-  [q, ret]
+h = {}
+p measure_ops(h) {
+  h.clear
 }
-
-def measure
-  n = 15
-  results = n.times.map do
-    input = {}
-
-    $go = false
-    $run = true
-    THREADS.each { |q,ret| q.push -> t { bench(input, t) } }
-    t0 = Process.clock_gettime(Process::CLOCK_MONOTONIC, :nanosecond)
-    $go = true
-
-    sleep 2
-
-    $run = false
-    results = THREADS.map { |q,ret| ret.pop }
-    t1 = Process.clock_gettime(Process::CLOCK_MONOTONIC, :nanosecond)
-    ops = results.reduce(:+)
-    dt = (t1-t0) / 1_000_000_000.0
-    ops /= dt
-    p ops
-    ops
-  end
-
-  [results.min, results.sort[results.size/2], results.max]
-  # results.min
-end
-
-p measure
