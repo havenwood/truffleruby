8a65eeae3ab349707d7a28973117e7634cb8c201
Arie Tal arietal@cs.technion.ac.il <arietal@cs.technion.ac.il>
2017-03-06 21:32:58 +0200

    gaussian and random general optimizations. Make bench_histogram use gaussian

--- bench/bench_histogram_ops.rb
+++ bench/bench_histogram_ops.rb
@@ -2,14 +2,16 @@ require_relative 'common'
 
 DATA_ELEMENTS = 10_000
 RAND_MAX = 1000
+SIGMA=Math.sqrt(RAND_MAX.to_f)
+MIDPOINT=RAND_MAX>>1
 
 def bench(hist, t)
-#  random = MyRandom.new(t)
+#  random = MyRandom.new(t*1111)
   random = Random.new(t)
   i = 0
   while i < DATA_ELEMENTS
-    r = random.next_int(RAND_MAX)
-#    r = (random.nextGaussian * RAND_MAX).to_i
+#    r = random.next_int(RAND_MAX)
+    r = (random.nextGaussian * SIGMA).to_i + MIDPOINT
     r = -r if r < 0
     hist[r] << i
     i += 1
@@ -19,5 +21,7 @@ end
 hist = put_if_absent_hash { Truffle::Array.set_strategy([], STRATEGY) }
 
 p measure_ops(hist) {
+  j=hist.keys.collect { |k| [k, hist[k].length] }.sort { |x,y| -(x[1] <=> y[1]) }
+  p "#{j[0]} --> #{j[-1]}, total: #{j.collect {|c| c[1]}.reduce(:+)}, populated: #{hist.keys.length}/#{RAND_MAX}"
   hist.clear
 }
