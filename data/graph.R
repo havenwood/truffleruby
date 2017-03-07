ca4eeaee8b182c00cbfafc7d312925842f21a967
Benoit Daloze <eregontp@gmail.com>
2017-03-07 17:50:01 +0100

    data

--- data/graph.R
+++ data/graph.R
@@ -23,10 +23,12 @@ full = load_data("conc_write_reads_sep.csv")
 full = load_data("conc_write_reads_sep_fair.csv")
 full = load_data("conc_write_reads_1numa.csv")
 full = load_data("conc_write_reads_1numa_16gb_heap.csv")
-full = load_data("conc_write_reads_2numa_16gb_heap.csv")
-full = load_data("conc_write_reads_2numa_10s_16gb_heap.csv")
-full = load_data("bench_array_conc_write_reads_arie.csv")
-full = load_data("java_write_read_ops.csv")
+# full = load_data("conc_write_reads_2numa_16gb_heap.csv")
+# full = load_data("conc_write_reads_2numa_10s_16gb_heap.csv")
+# full = load_data("bench_array_conc_write_reads_arie.csv")
+# full = load_data("java_write_read_ops.csv")
+# java = load_data("java_write_read_ops2numa.csv")
+# full = rbind(full, java)
 
 base_fixed = subset(full, VM=="FixedSize" & Threads=="1")$Value
 base_fll = subset(full, VM=="FastLayoutLock" & Threads=="1")$Value
@@ -39,20 +41,20 @@ base_fll = subset(full, VM=="FastLayoutLock" & Threads=="1")$Value
 # full = load_data("monte_carlo_pi_rb.csv")
 # full = load_data("monte_carlo_pi_sparc4x.csv")
 # full$Value = 1 / full$Value
-full$Value = full$Value / subset(full, Threads=="1")$Value
-base_fixed = 1
+# full$Value = full$Value / subset(full, Threads=="1")$Value
+# base_fixed = 1
 
 #png(file = "times.png", width=740, height=400)
 ggplot(data = full, aes(x=Threads, y=Value, group=VM, color=VM)) + geom_point() + geom_line() +
   xlab("Threads") + ylab("Throughput") +
-  scale_x_continuous(breaks = c(0, 2^(0:6), 12, 24, 48), minor_breaks = NULL, limits = c(0, max(full$Threads))) +
+  scale_x_continuous(breaks = c(0, 2^(0:6), 12, 24, 36, 48), minor_breaks = NULL, limits = c(0, max(full$Threads))) +
   scale_y_continuous(limits = c(0, max(full$Value))) +
   #scale_y_continuous(breaks = seq(0, max(full$Value), 10000)) +
   theme(text = element_text(size=20), legend.position="bottom",
         legend.title = element_blank(), legend.background = element_blank(), legend.key = element_blank(),
         legend.text = element_text(size = 16)) +
-  geom_abline(slope = base_fixed)
-#   geom_abline(slope = base_fll)
+  geom_abline(slope = base_fixed) +
+  geom_abline(slope = base_fll)
   
 #   scale_y_continuous(breaks = c(seq(0, 200, 30), 20, 40), minor_breaks = NULL) +
 #   scale_x_continuous(breaks = seq(0, 3000, 600)) +
