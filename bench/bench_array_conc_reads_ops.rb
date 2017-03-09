69763dd1045fcbf54ec26c29bdd15d0f87f13f56
Arie  Tal arietal@cs.technion.ac.il <arietal@cs.technion.ac.il>
2017-03-09 06:42:08 +0200

    Some global FLL measurements

---
+++ bench/bench_array_conc_reads_ops.rb
@@ -0,0 +1,31 @@
+require_relative 'common'
+
+# 1 write / READS reads
+# writes to the first N elements for per bench()
+
+N = 100
+READS = 1024
+raise if N > READS
+
+ary = READS.times.to_a * N_THREADS
+SUM = READS.times.reduce(:+)
+
+def bench(ary, t)
+  base = t * READS
+  i = 0
+  while i < N
+    j = base
+    last = base + READS
+    sum = 0
+    while j < last
+      sum += ary[j]
+      j += 1
+    end
+    i += 1
+  end
+  raise sum.to_s unless sum == SUM
+  sum
+end
+
+Truffle::Array.set_strategy(ary, STRATEGY)
+p measure_ops(ary) 
