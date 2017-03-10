674f33eba7b7090d945ab16e2e8a0ed2914c6014
Arie  Tal arietal@cs.technion.ac.il <arietal@cs.technion.ac.il>
2017-03-10 10:56:27 +0200

    Add the 90% reads, 10% writes benchmark

---
+++ bench/bench_array_conc_write_reads_ops_10_90.rb
@@ -0,0 +1,38 @@
+require_relative 'common'
+
+# 1 write / READS reads
+# writes to the first N elements for per bench()
+
+N = 100
+READS = 1024 # 90 % reads
+WRITES = 102 # 10 % writes
+raise if N > READS
+
+ary = READS.times.to_a * N_THREADS
+SUM = READS.times.reduce(:+)
+
+def bench(ary, t)
+  base = t * READS
+  i = 0
+  while i < N
+#    ary[base+i] = i
+
+    j = base
+    last = base + READS
+    sum = 0
+    while j < last
+      if ((j-base) % WRITES == 0) 
+         ary[j] = sum  
+      else
+         sum += ary[j]
+      end
+      j += 1
+    end
+    i += 1
+  end
+#  raise sum.to_s unless sum == SUM
+  sum
+end
+
+Truffle::Array.set_strategy(ary, STRATEGY)
+p measure_ops(ary) 
