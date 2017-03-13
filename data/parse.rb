bec18306809d51dfd503c6f57f2d6e98f4bfc837
Benoit Daloze <eregontp@gmail.com>
2017-03-13 15:15:19 +0100

    Automatically find output file for parse.rb

--- data/parse.rb
+++ data/parse.rb
@@ -1,15 +1,20 @@
-c = File.readlines(ARGV[0])
+file = ARGV[0]
+out = File.basename(file, ".*") + ".csv"
+
+c = File.readlines(file)
 c.reject! { |line|
   line.start_with?('$ ') ||
   line.include?('Running export ')
 }
 c = c.slice_before(/\[INFO\]/).to_a
 
-c.each { |run|
-  bench, name, threads = run.shift.split[-3..-1]
-  bench = File.basename(bench)
-  threads = Integer(threads)
-  run.pop while run.last.start_with?('[GC')
-  median = eval(run.last)[1]
-  puts [bench, threads, name, median].join(';')
-}
+File.open(out, "w") do |f|
+  c.each { |run|
+    bench, name, threads = run.shift.split[-3..-1]
+    bench = File.basename(bench)
+    threads = Integer(threads)
+    run.pop while run.last.start_with?('[GC')
+    median = eval(run.last)[1]
+    f.puts [bench, threads, name, median].join(';')
+  }
+end
