4d66efc57a592b8aa26160425b3aad57d506f2fc
Benoit Daloze <eregontp@gmail.com>
2017-03-14 14:17:24 +0100

    results OK except slow 1 thread fixed

--- data/graph.R
+++ data/graph.R
@@ -40,7 +40,7 @@ full = load_data("conc_write_reads_1_64_goliath_global_fll1.csv")
 full = load_data("conc_write_reads_reads_10_90_1_64_goliath_global_fll1.csv")
 full = load_data("conc_write_reads_10_90_1.csv")
 full = load_data("conc_write_reads_10_90_2.csv")
-full = load_data("conc_write_reads_10_90_4debug.csv")
+full = load_data("conc_write_reads_10_90_5.csv")
 
 
 base = max(subset(full, Threads=="1")$Value)
