c5304b2f2560cd8e623172b3b05dae6f60a0c831
Benoit Daloze <eregontp@gmail.com>
2017-03-16 19:06:10 +0100

    Some padding

--- bench/bench_array_conc_write_ops.rb
+++ bench/bench_array_conc_write_ops.rb
@@ -6,10 +6,12 @@ N = 100
 CHUNK = 1000
 SLICE = CHUNK + 24 # Some padding to avoid false sharing
 
-ary = SLICE.times.to_a * N_THREADS
+ary = SLICE.times.to_a * (1 + N_THREADS + 1)
+# Pad to avoid the Array being too close to its backing array,
+# and other threads could force loading a cache line from the 1st thread
 
 def bench(ary, t)
-  base = t * SLICE
+  base = (1+t) * SLICE
   i = 0
   while i < N
     j = 0
