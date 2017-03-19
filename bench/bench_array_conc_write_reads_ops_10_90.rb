e2bae9b5f84f56b2f9dd94f49b959ecee181c4aa
Benoit Daloze <eregontp@gmail.com>
2017-03-19 20:52:16 +0100

    Add some padding to all micro R/W

--- bench/bench_array_conc_write_reads_ops_10_90.rb
+++ bench/bench_array_conc_write_reads_ops_10_90.rb
@@ -7,10 +7,12 @@ CHUNK = 1000
 SLICE = CHUNK + 24 # Some padding to avoid false sharing
 WRITES_MODULO = 100 / WRITE_PERCENTS
 
-ary = SLICE.times.to_a * N_THREADS
+ary = SLICE.times.to_a * (1 + N_THREADS + 1)
+# Pad to avoid the Array being too close to its backing array,
+# and other threads could force loading a cache line from the 1st thread
 
 def bench(ary, t)
-  base = t * SLICE
+  base = (1+t) * SLICE
   i = 0
   while i < N
     sum = 0
