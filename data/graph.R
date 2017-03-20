947fc599e1a649e7b0bde4109960b1607b44fa38
Benoit Daloze <eregontp@gmail.com>
2017-03-20 17:24:52 +0100

    Change parse.rb to output values for all iterations

--- data/graph.R
+++ data/graph.R
@@ -7,54 +7,62 @@ library(ggplot2)
 library(plyr)
 
 load_data <- function(filename) {
-  d = read.csv(filename, header=FALSE, col.names=c("Bench", "Threads", "VM", "Value"), sep=";")
+  d = read.csv(filename, header=FALSE, col.names=c("Bench", "Threads", "VM", "Iteration", "Value"), sep=";")
   n = length(d$Value)
   d$VM = factor(d$VM)
   d$VM = revalue(d$VM, c("FastLayoutLock"="LightweightLayoutLock"))
-  #d$Iteration = seq(from = 1, to = n)
-  d
+  
+  # Remove warmup
+  no_warmup = subset(d, d$Iteration!=1)
+
+  with_median = ddply(no_warmup, .(Bench, Threads, VM), transform,
+                      Median = median(Value),
+                      Min = min(Value),
+                      Max = max(Value))
+
+  with_median
 }
 
-full = load_data("conc_appends_1_32.csv")
-full = load_data("conc_appends_1_64.csv")
-full = load_data("conc_write_reads_ops_1_64.csv")
-full = load_data("conc_write_reads_ops_v2_1_64.csv")
-
-full = load_data("conc_write_reads_sep.csv")
-full = load_data("conc_write_reads_sep_fair.csv")
-full = load_data("conc_write_reads_1numa.csv")
-full = load_data("conc_write_reads_1numa_16gb_heap.csv")
-# full = load_data("conc_write_reads_2numa_16gb_heap.csv")
-# full = load_data("conc_write_reads_2numa_10s_16gb_heap.csv")
-# full = load_data("bench_array_conc_write_reads_arie.csv")
-# full = load_data("java_write_read_ops.csv")
-# java = load_data("java_write_read_ops2numa.csv")
-# full = rbind(full, java)
-full = load_data("conc_write_reads_ops_1numa.csv")
-full = load_data("conc_write_reads_ops_2numa.csv")
-full = load_data("conc_write_reads_ops_2numa_all.csv")
-full = load_data("conc_write_reads_ops_2numa_2.csv")
-full = load_data("conc_write_reads_ops_2numa_3.csv")
-full = load_data("conc_write_reads_1_64_goliath.csv")
-full = load_data("conc_write_reads_1_64_goliath_global_fll.csv")
-full = load_data("conc_write_reads_1_64_goliath_global_fll1.csv")
-full = load_data("conc_write_reads_reads_10_90_1_64_goliath_global_fll1.csv")
-full = load_data("conc_write_reads_10_90_1.csv")
-full = load_data("conc_write_reads_10_90_2.csv")
-full = load_data("conc_write_reads_10_90_5.csv")
-full = load_data("conc_write_reads_10_90_6.csv")
-full = load_data("conc_write_ops_1.csv")
-full = load_data("conc_write_ops_2.csv")
-full = load_data("conc_write_reads_10_90_8.csv")
-full = load_data("conc_reads_ops1.csv")
-full = load_data("conc_reads_ops2.csv")
+# full = load_data("conc_appends_1_32.csv")
+# full = load_data("conc_appends_1_64.csv")
+# full = load_data("conc_write_reads_ops_1_64.csv")
+# full = load_data("conc_write_reads_ops_v2_1_64.csv")
+# 
+# full = load_data("conc_write_reads_sep.csv")
+# full = load_data("conc_write_reads_sep_fair.csv")
+# full = load_data("conc_write_reads_1numa.csv")
+# full = load_data("conc_write_reads_1numa_16gb_heap.csv")
+# # full = load_data("conc_write_reads_2numa_16gb_heap.csv")
+# # full = load_data("conc_write_reads_2numa_10s_16gb_heap.csv")
+# # full = load_data("bench_array_conc_write_reads_arie.csv")
+# # full = load_data("java_write_read_ops.csv")
+# # java = load_data("java_write_read_ops2numa.csv")
+# # full = rbind(full, java)
+# full = load_data("conc_write_reads_ops_1numa.csv")
+# full = load_data("conc_write_reads_ops_2numa.csv")
+# full = load_data("conc_write_reads_ops_2numa_all.csv")
+# full = load_data("conc_write_reads_ops_2numa_2.csv")
+# full = load_data("conc_write_reads_ops_2numa_3.csv")
+# full = load_data("conc_write_reads_1_64_goliath.csv")
+# full = load_data("conc_write_reads_1_64_goliath_global_fll.csv")
+# full = load_data("conc_write_reads_1_64_goliath_global_fll1.csv")
+# full = load_data("conc_write_reads_reads_10_90_1_64_goliath_global_fll1.csv")
+# full = load_data("conc_write_reads_10_90_1.csv")
+# full = load_data("conc_write_reads_10_90_2.csv")
+# full = load_data("conc_write_reads_10_90_5.csv")
+# full = load_data("conc_write_reads_10_90_6.csv")
+# full = load_data("conc_write_ops_1.csv")
+# full = load_data("conc_write_ops_2.csv")
+# full = load_data("conc_write_reads_10_90_8.csv")
+# full = load_data("conc_reads_ops1.csv")
+# full = load_data("conc_reads_ops2.csv")
 full = load_data("conc_reads_ops3.csv")
 # full = load_data("conc_write_reads_50_50_1.csv")
 # full = load_data("conc_write_reads_50_50_2.csv")
 
 base = max(subset(full, Threads=="1")$Value)
 # base = max(subset(full, Threads=="2")$Value)/2
-base_fll = subset(full, VM=="LightweightLayoutLock" & Threads=="1")$Value
+base_fll = max(subset(full, VM=="LightweightLayoutLock" & Threads=="1")$Value)
 # full$Value = full$Value / base
 
 # full = subset(full, VM != "FixedSize")
@@ -69,16 +77,19 @@ base_fll = subset(full, VM=="LightweightLayoutLock" & Threads=="1")$Value
 # base = base_fll = 1
 
 #png(file = "times.png", width=740, height=400)
-ggplot(data = full, aes(x=Threads, y=Value, group=VM, color=VM)) + geom_point() + geom_line() +
+ggplot(data = full, aes(x=Threads, y=Median, group=VM, color=VM)) +
+  geom_abline(size=0.2, slope = base) +
+  geom_abline(size=0.2, slope = base_fll) +
+  geom_point(size=2, aes(shape=VM)) +
+  geom_line(size=0.7) +
+  # geom_errorbar(aes(ymin=Min, ymax=Max)) +
   xlab("Threads") + ylab("Throughput") +
   scale_x_continuous(breaks = c(0, 1, 2, 4, 8, 12, 16, 18, 20, 24, 28, 32, 36, 48), minor_breaks = NULL, limits = c(0, max(full$Threads))) +
   scale_y_continuous(limits = c(0, max(full$Value))) +
   #scale_y_continuous(breaks = seq(0, max(full$Value), 10000)) +
   theme(text = element_text(size=20), legend.position="bottom",
         legend.title = element_blank(), legend.background = element_blank(), legend.key = element_blank(),
-        legend.text = element_text(size = 16)) +
-  geom_abline(slope = base) +
-  geom_abline(slope = base_fll)
+        legend.text = element_text(size = 16))
   
 #   scale_y_continuous(breaks = c(seq(0, 200, 30), 20, 40), minor_breaks = NULL) +
 #   scale_x_continuous(breaks = seq(0, 3000, 600)) +
--- data/parse.rb
+++ data/parse.rb
@@ -13,8 +13,10 @@ File.open(out, "w") do |f|
     bench, name, threads = run.shift.split[-3..-1]
     bench = File.basename(bench)
     threads = Integer(threads)
-    run.pop while run.last.start_with?('[GC')
-    median = eval(run.last)[1]
-    f.puts [bench, threads, name, median].join(';')
+    run = run.reject { |line| line.start_with?('[') }
+    times = run.map { |line| Float(line) }
+    times.each.with_index(1) { |time, i|
+      f.puts [bench, threads, name, i, time].join(';')
+    }
   }
 end
