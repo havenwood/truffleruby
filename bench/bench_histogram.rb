b99716197c178452255fef63126b49ef470072fc
Benoit Daloze <eregontp@gmail.com>
2017-03-28 15:37:25 +0200

    Some extra code for testing

--- bench/bench_histogram.rb
+++ bench/bench_histogram.rb
@@ -3,7 +3,7 @@ require_relative 'common'
 # Fixed workload distributed between threads
 
 DATA_ELEMENTS = 4_000_000
-RAND_MAX = 10_000
+RAND_MAX = 10_000 # 65_536
 
 def bench(hist, t)
   from, to = thread_partition(t, DATA_ELEMENTS)
@@ -20,5 +20,6 @@ hist = put_if_absent_hash { Truffle::Array.set_strategy([], STRATEGY) }
 
 p measure_one_op(hist) {
   hist.clear
+  # RAND_MAX.times { |i| hist[i] }
 }
 
